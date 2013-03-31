00dcfe6ea654045c0beab2f76785e3ad2139f808
Devon Jorlett <djorlett@hotmail.com>
2013-03-31 02:02:37 -0500

    main menu additions

---
+++ MightyFights_Prototype/MightyFights_Prototype/Game Objects/BasicSprite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using MightyFights_Support;
+
+namespace MightyFights_Prototype
+{
+	public class BasicSprite : IDrawable
+	{
+		public Texture2D cTexRef { get; set; }
+		public Vector2 tPos { get; set; }
+		public Frame cFrame { get; set; }
+	}
+}
--- MightyFights_Prototype/MightyFights_Prototype/Scenes/OutGame/MainMenu.cs
+++ MightyFights_Prototype/MightyFights_Prototype/Scenes/OutGame/MainMenu.cs
@@ -21,6 +21,8 @@ namespace MightyFights_Prototype
 		AnimationData	_cTroopers;
 		SpriteBatch		_cBatch;
 		GraphicsDevice	_cGraphics;
+		BasicSprite		_cPlayer1, 
+						_cPlayer2;
 		
 		#region IGameScene Members
 
@@ -36,9 +38,17 @@ namespace MightyFights_Prototype
 			_cGraphics.Clear(Color.Black);
 
 			_cBatch.Begin();
-				_cBatch.Draw(_cCardP1, new Vector2(300, 300), _cCardP1.Bounds, Color.White);
-				_cBatch.Draw(_cCardP2, new Vector2(500, 300), _cCardP2.Bounds, Color.White);
-				_cBatch.Draw(_cToBattle, new Vector2(300, 500), _cToBattle.Bounds, Color.White);
+				_cBatch.Draw(_cCardP1, new Vector2(_cGraphics.Viewport.Width / 2 - _cToBattle.Bounds.Width, 400), _cCardP1.Bounds, Color.White);
+				_cBatch.Draw(_cCardP2, new Vector2(_cGraphics.Viewport.Width / 2 + _cToBattle.Bounds.Width - _cCardP2.Width, 400), _cCardP2.Bounds, Color.White);
+				_cBatch.Draw(_cToBattle, new Vector2(_cGraphics.Viewport.Width / 2 - (_cToBattle.Bounds.Width / 2), 400 + _cCardP1.Bounds.Height / 2 - (_cToBattle.Bounds.Height / 2)), _cToBattle.Bounds, Color.White);
+				//spriteBatch.Draw(_cSpriteSheet, tVector, cFrame.tRect, Color.White, 
+				//    cFrame.bRot ? -(float)Math.PI/2 : 0, cFrame.tTopLeft, 1, SpriteEffects.None, 0);
+
+				_cBatch.Draw(_cTrooperTex, _cPlayer1.tPos, _cPlayer1.cFrame.tRect, Color.White, 
+					_cPlayer1.cFrame.bRot ? -(float)Math.PI/2 : 0, _cPlayer1.cFrame.tTopLeft, 1, SpriteEffects.None, 0);
+				_cBatch.Draw(_cTrooperTex, _cPlayer2.tPos, _cPlayer2.cFrame.tRect, Color.White, 
+					_cPlayer2.cFrame.bRot ? -(float)Math.PI/2 : 0, _cPlayer2.cFrame.tTopLeft, 1, SpriteEffects.None, 0);
+
 			_cBatch.End();
 		}
 
@@ -54,6 +64,20 @@ namespace MightyFights_Prototype
 				_cTrooperTex = cContent.Load<Texture2D>(@"Sprite Data\Troopers\Halberd\Front Facing\Guys");
 			
 				_cTroopers = cContent.Load<AnimationData>(@"Sprite Data\Troopers\Halberd\Front Facing\GuysArray");
+
+				_cPlayer1 = new BasicSprite();
+				_cPlayer1.cFrame = _cTroopers.caFrameData[_cTroopers.cReferenceList["FrontFacing"]["Colors"]["azure"].iStartIndex];
+				_cPlayer1.cTexRef = _cTrooperTex;
+				_cPlayer1.tPos = new Vector2(_cGraphics.Viewport.Width / 2 - _cToBattle.Bounds.Width + 
+												_cCardP1.Width / 2 - _cPlayer1.cFrame.tRect.Width / 2, 400 + _cCardP1.Bounds.Width / 2 - 
+												_cPlayer1.cFrame.tRect.Height / 2);
+
+				_cPlayer2 = new BasicSprite();
+				_cPlayer2.cFrame = _cTroopers.caFrameData[_cTroopers.cReferenceList["FrontFacing"]["Colors"]["blood zombie"].iStartIndex];
+				_cPlayer2.cTexRef = _cTrooperTex;
+				_cPlayer2.tPos = new Vector2(_cGraphics.Viewport.Width / 2 + _cToBattle.Bounds.Width - _cCardP2.Width + 
+												_cPlayer2.cFrame.tRect.Width / 2, 400 + _cCardP2.Bounds.Width / 2 - 
+												_cPlayer2.cFrame.tRect.Height / 2);
 
 				_cBatch = new SpriteBatch(DataStore.cInstance.cGraphics);
 
